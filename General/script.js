79dee06b662545742f2f2ba8cd70ff6cadcd1713
cfranco-11 <caf9vasquez@gmail.com>
2024-03-05 09:16:47 -0500

    Se sacan archivos HTML

--- General/script.js
+++ General/script.js
@@ -55,12 +55,12 @@ function validateFormLogin(){
     if(cedula == 1 && password == 1){
         setTimeout(function() {
             // Cambia la URL a la que quieres redirigirte
-            window.location.href = "/Teachers/HomeTeachers/indexTeachersHome.html"
+            window.location.href = "RiwiMindset/indexTeachersHome.html"
           }, 100); // 10000 milisegundos = 10 segundos
         }else if(cedula == 2 && password == 2){
             setTimeout(function() {
                 // Cambia la URL a la que quieres redirigirte
-                window.location.href = "/Students/HomeStudents/indexHomeEstudents.html"
+                window.location.href = "RiwiMindset/indexHomeEstudents.html"
               }, 100); // 10000 milisegundos = 10 segundos
             }
         
